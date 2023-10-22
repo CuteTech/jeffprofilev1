9ce8db246e5ce2965a10824ab142f61b014ab101
Tonia Roganti <tonia@chariotmove.com>
2023-10-22 16:15:08 -0400

    initial setup

---
+++ app/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="text-center text-gray-400">
+      &copy; {new Date().getFullYear()} JeffAhnNYC.com All Rights Reserved.
+    </div>
+  );
+}
+
+export default Footer;
