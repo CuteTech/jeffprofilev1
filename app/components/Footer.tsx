9ed02e56c66bea8ccf5a7dfe284d8adfe8c37290
Tonia Roganti <tonia@chariotmove.com>
2023-10-22 16:56:57 -0400

    padding footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <div className="text-center text-gray-400">
+    <div className="text-center text-gray-400 p-8">
       &copy; {new Date().getFullYear()} JeffAhnNYC.com All Rights Reserved.
     </div>
   );
